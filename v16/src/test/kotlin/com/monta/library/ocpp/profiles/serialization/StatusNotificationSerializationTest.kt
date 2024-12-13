f82f815c7d790815b018a8307b19e73daff34adf
Morten Andersen <ma@monta.app>
2024-12-13 16:16:15 +0100

    ignore: Adding tests that shows we break if we get StatusNotifications with timestamps without timezone

--- v16/src/test/kotlin/com/monta/library/ocpp/profiles/serialization/StatusNotificationSerializationTest.kt
+++ v16/src/test/kotlin/com/monta/library/ocpp/profiles/serialization/StatusNotificationSerializationTest.kt
@@ -5,6 +5,7 @@ import com.monta.library.ocpp.common.serialization.Message
 import com.monta.library.ocpp.common.serialization.MessageSerializer
 import com.monta.library.ocpp.common.serialization.ParsingResult
 import com.monta.library.ocpp.common.serialization.SerializationMode
+import com.monta.library.ocpp.v16.core.StatusNotificationRequest
 import com.monta.library.ocpp.v16.error.OcppErrorResponderV16
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -23,6 +24,34 @@ class StatusNotificationSerializationTest : StringSpec({
         ocppMessage.uniqueId shouldBe "78ee4811-a044-417e-9c70-b51c2c296bf9"
         ocppMessage.action shouldBe "StatusNotification"
         ocppMessage.payload shouldBe TestUtils.toJsonNode("{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Available\"}")
+    }
+
+    "parse a valid status notification request with a timestamp" {
+        val jsonString = TestUtils.getFileAsString("status_notification/req-timestamp.json")
+        val parsingResult = messageSerializer.parse(jsonString)
+
+        parsingResult.shouldBeInstanceOf<ParsingResult.Success<Message.Request>>()
+        val ocppMessage = parsingResult.value
+        ocppMessage.shouldBeInstanceOf<Message.Request>()
+        val expected = messageSerializer.deserializePayload(ocppMessage, StatusNotificationRequest::class.java)
+        expected.shouldBeInstanceOf<ParsingResult.Success<StatusNotificationRequest>>()
+        ocppMessage.uniqueId shouldBe "2024120922534800000049"
+        ocppMessage.action shouldBe "StatusNotification"
+        ocppMessage.payload shouldBe TestUtils.toJsonNode("{\"connectorId\":1,\"status\":\"Available\",\"errorCode\":\"NoError\",\"info\":\"eCp_12V\",\"timestamp\":\"2024-12-09T22:53:49.000Z\"}")
+    }
+
+    "parse a valid status notification request with a timestamp without a timezone" {
+        val jsonString = TestUtils.getFileAsString("status_notification/req-timestamp-no-timezone.json")
+        val parsingResult = messageSerializer.parse(jsonString)
+
+        parsingResult.shouldBeInstanceOf<ParsingResult.Success<Message.Request>>()
+        val ocppMessage = parsingResult.value
+        ocppMessage.shouldBeInstanceOf<Message.Request>()
+        val expected = messageSerializer.deserializePayload(ocppMessage, StatusNotificationRequest::class.java)
+        expected.shouldBeInstanceOf<ParsingResult.Success<StatusNotificationRequest>>()
+        ocppMessage.uniqueId shouldBe "2024120922534800000049"
+        ocppMessage.action shouldBe "StatusNotification"
+        ocppMessage.payload shouldBe TestUtils.toJsonNode("{\"connectorId\":1,\"status\":\"Available\",\"errorCode\":\"NoError\",\"info\":\"eCp_12V\",\"timestamp\":\"2024-12-09T22:53:49.000\"}")
     }
 
     "parse a valid status notification response" {
